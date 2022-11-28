28066d53799c439f355cb94c544f2db4317fcb65
Chirapon Hemtrakan <Chiraponkub@hotmail.com>
2022-11-28 22:13:36 +0700

    test git

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SosApp',
+      title: 'SosApps',
       theme: ThemeData(primarySwatch: Colors.red),
       home: Home(),
     );
